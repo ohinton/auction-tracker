2afebaa71a83c87703c8070c4e2cd2ca63bc8930
Olivia Hinton <hinton.olivia@gmail.com>
2016-09-23 16:26:30 -0700

    test add a donor spec but still failing

--- spec/features/add_donor_pages_spec.rb
+++ spec/features/add_donor_pages_spec.rb
@@ -3,11 +3,10 @@
 # describe "the add a donor process", js: true do
 #   it "adds a new donor" do
 #     user = FactoryGirl.create(:user)
-#     login_as(user, :scope => :user)
 #     volunteer = FactoryGirl.create(:volunteer)
+#     login_as user
 #     visit volunteers_path
 #     click_link 'Add a new donor'
-#     binding.pry
 #     fill_in 'Name', :with => 'Test Name'
 #     fill_in 'Phone', :with => '503-000-0000'
 #     fill_in 'Email', :with => 'test@email.com'
@@ -15,8 +14,6 @@
 #     fill_in 'City', :with => 'Portland'
 #     fill_in 'State', :with => 'OR'
 #     fill_in 'Zip', :with => '97210'
-#
-#     find("#donor_volunteer_id", :visible => false).value
 #
 #     click_on 'Create Donor'
 #     expect(page).to have_content 'Donors'
--- spec/features/view_volunteers_pages_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-# describe "the view volunteers process" do
-#   it "lists the volunteers on the index page" do
-#     volunteer = FactoryGirl.create(:volunteer)
-#     visit volunteers_path
-#     expect(page).to have_content 'Test Volunteer'
-#   end
-# end
