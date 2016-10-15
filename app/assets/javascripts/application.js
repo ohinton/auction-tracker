5d9b8398a20bce89dc6b88df9cd95e55eb0f46d0
Olivia Hinton <hinton.olivia@gmail.com>
2016-10-14 18:53:55 -0700

    add conditional statement in application file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 //= require materialize-sprockets
 //= require underscore
 //= require gmaps/google
+//= require turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # root to: 'volunteers#show'
   root to: 'home#index'
 
   resources :volunteers
-  
+
   resources :donors do
     resources :updates
   end
