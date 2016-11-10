cab16af9e30bb9094ad66974cc37ed9120f1877c
Olivia Hinton <hinton.olivia@gmail.com>
2016-11-09 22:29:55 -0800

    remove @s3_direct_post variable from volunteers controller

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,6 +1,4 @@
 class VolunteersController < ApplicationController
-  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
-
   before_action :authenticate_user!
 
   def show
@@ -22,10 +20,6 @@ class VolunteersController < ApplicationController
   private
   def volunteer_params
     params.require(:volunteer).permit(:name, :first_name, :last_name, :phone, :address, :city, :state, :zip, :user_id, :photo)
-  end
-  
-  def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
   end
 
 end
