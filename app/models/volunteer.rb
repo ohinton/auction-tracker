bc9546be1e21fc9d4752f7f2c473db13cb5e6396
Olivia Hinton <hinton.olivia@gmail.com>
2016-11-10 08:45:08 -0800

    fix missing.png link in volunteer model

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,7 +2,7 @@ class Volunteer < ActiveRecord::Base
   belongs_to :user
   has_many :donors
 
-  has_attached_file :photo, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "https://s3-us-west-2.amazonaws.com/auctionary/volunteers/photos/000/000/001/thumb/missing.png"
+  has_attached_file :photo, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "//s3.amazonaws.com/auctionary/volunteers/photos/000/000/001/thumb/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
 end
